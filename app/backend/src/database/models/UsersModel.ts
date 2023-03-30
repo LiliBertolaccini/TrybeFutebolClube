995f1e957de2032644d2572db12f0156d3e68679
Lilibertola <bertolaccini55@gmail.com>
2023-03-29 21:24:17 -0300

    ok 6

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,28 @@
+import { DataTypes, InferAttributes, Model } from 'sequelize';
+import database from './index';
+
+export default class User extends Model<InferAttributes<User>> {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.NUMBER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  sequelize: database,
+  timestamps: false,
+  modelName: 'users',
+  underscored: true,
+});
